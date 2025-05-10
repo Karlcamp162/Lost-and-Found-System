b23a94589d0860eafe15a8a9d41629d2aee051e6
Karlcamp162 <ckarlcamp@gmail.com>
2025-05-10 15:11:27 +0800

    update bottom navigation widget

--- lib/components/bottom_navigation_widget.dart
+++ lib/components/bottom_navigation_widget.dart
@@ -10,13 +10,19 @@ class BottomNavigationWidget extends StatefulWidget {
 class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home)),
-        BottomNavigationBarItem(icon: Icon(Icons.notifications)),
-        BottomNavigationBarItem(icon: Icon(Icons.chat)),
-        BottomNavigationBarItem(icon: Icon(Icons.person)),
-      ],
+    return BottomAppBar(
+      notchMargin: 8.0,
+      shape: CircularNotchedRectangle(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(icon: Icon(Icons.home), onPressed: () {}),
+          IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+          SizedBox(width: 40.0),
+          IconButton(icon: Icon(Icons.chat), onPressed: () {}),
+          IconButton(icon: Icon(Icons.person), onPressed: () {}),
+        ],
+      ),
     );
   }
 }
