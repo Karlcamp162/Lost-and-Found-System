f5059eb7116d03a31d55a3bd306892406cce2b84
AngelAlaba13 <aklalaba@nemsu.edu.ph>
2025-05-12 00:51:02 +0800

    angel - profile

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,12 +14,16 @@ import 'package:lost_and_found_system/profileNavigations/sendFeedback.dart';
 
 class Home extends StatefulWidget {
   final String currentUserName;
-  final String currentStudentId; // Added this line to pass studentId
+  final String currentStudentId;
+  final String studentDepartmentName;
+  final String studentCourseName; // Added this line to pass studentId
 
   const Home({
     super.key,
     required this.currentUserName,
     required this.currentStudentId,
+    required this.studentDepartmentName,
+    required this.studentCourseName,
   });
 
   @override
@@ -31,6 +35,8 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   String get currentUser => widget.currentUserName;
   String get currentStudent => widget.currentStudentId; // Access studentId
+  String get studentDepartment => widget.studentDepartmentName;
+  String get studentCourse => widget.studentCourseName;
 
   final ScrollController _scrollController = ScrollController();
   String? selectedPostId;
@@ -303,6 +309,8 @@ class _HomeState extends State<Home> {
         return Profile(
           currentUserName: currentUser,
           studentId: currentStudent,
+          studentDepartmentName: studentDepartment,
+          studentCourseName: studentCourse,
         ); // Pass studentId to Profile
       default:
         return _buildHomeTab();
